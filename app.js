c942410e1198fb347a2e5d7e404ca6f582e6ccb3
DAVELEON23 <davidaarondeleon@gmail.com>
2023-11-09 23:41:11 -0300

    se agrego registro y login de ML. Archivo general-home-login-registro.css. Archivo login-register.html

--- app.js
+++ app.js
@@ -5,9 +5,31 @@ const path = require ("path")
 
 app.use(express.static(path.join(__dirname,"public")))
 
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
 app.get ("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/home.html"))
 });
+
+app.get ("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+});
+app.post("/login", (req,res) => {
+    console.log(req.body);
+res.redirect("/");
+})
+
+app.get ("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+});
+app.post("/register", (req,res) => {
+    console.log(req.body);
+res.redirect("/");
+})
 
 app.listen (4000,()=> {
     console.log("servidor corriendo http://localhost:4000/")
